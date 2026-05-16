e4c4b2e25372f4e7055a049b4b7470758a2af3d7
KipperSou <2559208327@qq.com>
2026-05-16 19:09:18 +0800

    style: refine avatar presentation on homepage

--- src/components/header/AnimatedLogo.tsx
+++ src/components/header/AnimatedLogo.tsx
@@ -27,9 +27,9 @@ export function AnimatedLogo() {
 function Logo() {
   return (
     <a className="block" href={withBase('/')} title="Nav to home">
-      <div className="size-[40px] rounded-2xl overflow-hidden border border-zinc-200 bg-white dark:border-zinc-700 dark:bg-slate-900">
+      <div className="size-[40px] rounded-2xl overflow-hidden border border-zinc-200/80 bg-white dark:border-zinc-700 dark:bg-slate-900">
         <img
-          className="size-full select-none object-contain object-center p-1"
+          className="size-full select-none object-cover object-top scale-[1.35]"
           src={withBase(author.avatar)}
           alt="Site owner avatar"
         />
